3ad782483c3f02dadcf7e6ee6a5b1e75027847ea
Atara Rose <ataraeg613@gmail.com>
2023-06-28 16:15:35 +0300

    uploads display done!!!! fix date...

--- src/comps/upload_comps/uploadItem.js
+++ src/comps/upload_comps/uploadItem.js
@@ -1,15 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { API_URL, doApiMethod } from '../../services/service';
+import React, { useState} from 'react'
 import { Modal } from 'react-bootstrap';
-import { format } from 'date-fns';
 
-import { Avatar, Button, IconButton, Zoom } from "@mui/material";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Avatar, Button, IconButton, Popover } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import VisibilityIcon from '@mui/icons-material/Visibility';
-import { toast } from "react-toastify";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Event, FmdGood, Sell } from '@mui/icons-material';
 import "./uploadItem.css";
 
 export default function UploadItem(props) {
@@ -19,6 +14,12 @@ export default function UploadItem(props) {
     const handleShow = () => setShow(true);
     const [datePublished, setDatePublished] = useState(Date(item.date_created));
     const [isHovered, setIsHovered] = useState(false);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const isFieldEmpty = (_field) => {
+        return _field == "" || _field == undefined || _field == null;
+    }
 
     return (
         <div className="mainDiv p-0">
@@ -73,30 +74,88 @@ export default function UploadItem(props) {
                     <div className='text-muted ms-1'>{item.price} NIS</div>
                 </div>
             </div>
-            <Modal show={show} onHide={handleClose} centered>
+            <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>{item.bookId.name}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <div> <strong>upload info:</strong>
-                        <div>picture: {item.img_url}</div>
-                        <div>Info: {item.info}</div>
-                        <div>Date created: {datePublished}</div>
-                        <div>price: {item.price}</div>
+                    <div style={{ position: 'relative' }} className='row'>
+                        <div className='col-7 p-0 overflow-hidden'>
+                            <img className='imgBookInfo' style={{ borderRadius: "12px" }} src={item.img_url} alt="bookImg" />
+                            <div>
+                                <div className="mt-2 ms-1 text-start">{item.bookId.name}</div>
+                            </div>
+                        </div>
+                        <div className='col-5 mx-auto'>
+                            <div className="mt-1 mx-auto">
+                                <Avatar className="mt-1 mx-auto"
+                                    sx={{ float: "start", width: 63, height: 63 }}
+                                    src="/images/man.png"
+                                    alt="AvatarOfBook"
+                                />
+                                <div style={{ fontWeight: 500 }} className="h5 mt-1 text-center dark">
+                                    {item.user_id.fullName.firstName} {item.user_id.fullName.lastName}
+                                </div>
+                                <div className="mt-1 d-flex justify-content-center">
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={(event) => {
+                                            setAnchorEl(event.currentTarget);
+                                        }}
+                                    >
+                                        Contact info
+                                    </Button>
+                                    <Popover
+                                        anchorEl={anchorEl}
+                                        open={open}
+                                        id={open ? "simple-popover" : undefined}
+                                        onClose={() => {
+                                            setAnchorEl(null);
+                                        }}
+                                        transformOrigin={{
+                                            vertical: 'top',
+                                            horizontal: 'center',
+                                        }}
+                                        anchorOrigin={{
+                                            vertical: 'bottom',
+                                            horizontal: 'center',
+                                        }}
+                                    >
+                                        <div className="h6 mt-2 ms-1"> Phone: {item.user_id.phone}</div>
+                                        <div className="h6 mt-2 ms-1"> Email: {item.user_id.email}</div>
+                                    </Popover>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className='d-flex align-items-center'>
+                            <div className="d-block d-md-flex text-center">
+                                <div> <FmdGood className='me-2' /></div>
+                                {item.user_id.city}
+                            </div>
+                            <hr className='hr' />
+                            <div className="d-block d-md-flex text-center">
+                                <div> <Sell className='me-2' /></div>
+                                <div>{item.price} NIS</div>
+                            </div>
+                            <hr className='hr' />
+                            <div className="d-block d-md-flex text-center">
+                                <div> <Event className='me-2' /></div>
+                                <div>{datePublished}</div>
+                            </div>
+                        </div>
                     </div>
-                    <div><strong>Book info:</strong>
-                        <div>publisher: {item.bookId.publisher}</div>
-                        <div>supervision: {item.bookId.supervision}</div>
-                        <div>author: {item.bookId.author_name}</div>
-                        <div>name: {item.bookId.name}</div>
-                        <div>type: {item.bookId.type}</div>
-                        <div>subject: {item.bookId.subject}</div>
-                    </div>
-                    <div><strong>contact info:</strong>
-                        <div> Name: {item.user_id.fullName.firstName} {item.user_id.fullName.lastName}</div>
-                        <div> Phone: {item.user_id.phone}</div>
-                        <div> Email: {item.user_id.email}</div>
-                        <div> City: {item.user_id.city}</div>
+                    <hr className='mt-1 mb-1' />
+                    <div >
+                        <div className='h4 '>About The Book</div>
+                        <div><strong>Title:</strong> {item.bookId.name}</div>
+                        <div><strong>Subject:</strong> {item.bookId.subject}</div>
+                        {!isFieldEmpty(item.bookId.type) && <div><strong>Type:</strong> {item.bookId.type}</div>}
+                        {!isFieldEmpty(item.bookId.supervision) && <div><strong>Supervision:</strong> {item.bookId.supervision}</div>}
+                        {!isFieldEmpty(item.bookId.author_name) && <div><strong>Author:</strong> {item.bookId.author_name}</div>}
+                        {!isFieldEmpty(item.bookId.publisher) && <div><strong>Publisher:</strong> {item.bookId.publisher}</div>}
+                        {!isFieldEmpty(item.info) && <div><strong>Additional Information:</strong> {item.info}</div>}
                     </div>
                 </Modal.Body>
                 <Modal.Footer>
@@ -105,7 +164,10 @@ export default function UploadItem(props) {
                     </Button>
                 </Modal.Footer>
             </Modal>
-        </div>
+        </div >
 
     )
 }
+
+
+
