6d7bbff42e3d8b16950f00b271131890fffafe24
Elisheva (Aronstam) Sherman <57481785+ElishAronstam@users.noreply.github.com>
2023-06-29 15:25:06 +0300

    popup for logout

--- src/comps/login_comps/logout.js
+++ src/comps/login_comps/logout.js
@@ -1,11 +1,13 @@
-import React, { useEffect,useContext } from 'react'
+import React, { useState,useEffect,useContext } from 'react'
 import { UserContext } from '../../App';
 import { useNavigate } from 'react-router-dom';
 import { TOKEN_NAME } from '../../services/service';
 import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-bootstrap';
+import "./logout.css"
 
 const Logout = () => {
+    const [showPopup, setShowPopup] = useState(false);
     const nav = useNavigate();
     const {isLogedIn,setLogedIn} = useContext(UserContext);
   
@@ -16,11 +18,23 @@ const Logout = () => {
         
         setLogedIn(false);
         localStorage.removeItem(TOKEN_NAME);
-        nav('/login')
+        setShowPopup(true)
+        // nav('/login')
 
     }
+    const handlePopupClose = () => {
+        setShowPopup(false);
+        nav('/login')
+      };
     return (
-        <div></div>
+        <div>
+            {showPopup && (
+        <div className="popup">
+          <p>You have successfully logged out!</p>
+          <button onClick={handlePopupClose}>Close</button>
+        </div>
+      )}
+        </div>
     )
 }
 
